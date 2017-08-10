1d0427bc798525459c9506e71f276f2d27cff149
GCordero1 <Gerson.Cordero@gmail.ccom>
2017-08-09 23:06:52 -0400

    added form reset

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var addItem = function(state, item) {
 	state.items.push(item);
 }
 var deleteItems = function(state, item) {
-	state.items.pop(item);
+	state.items.splice(item, 1);
 }
 
 // function that renders state
@@ -17,23 +17,30 @@ var renderList = function(state, element) {
 		return '<li><span class="shopping-item">' + item  +
 		'</span><div class="shopping-item-controls">' + 
           '<button class="shopping-item-toggle">' +
-            '<span class="button-label">check</span> </button>' +
+            '<span class="button-label">check</span></button>' +
           ' <button class="shopping-item-delete"><span class="button-label">delete</span>' +
-          '</button> </div>';
+          '</button> </div> </li>';
 	});
 	element.html(itemsHTML);
 };
 
-// event listeners
+// add items to list event listener
 $('#js-shopping-list-form').submit(function(event) {
 	event.preventDefault();
 	addItem(state, $('#shopping-list-entry').val());
 	renderList(state, $('.shopping-list'));
+	//form reset
+	this.reset();
 });
+
+// remove items from list event listener
 $('.shopping-item-delete').on('click', function(event) {
 	event.preventDefault();
-	deleteItem();
+	deleteItems(state, $('#shopping-list-entry'));
+	renderList(state, $('.shopping-list'));
 });
+
+// check items from list event listener
 $('.shopping-item-toggle').on('click', function(event) {
 	event.preventDefault();
 	$(this).toggleClass('.shopping-item__checked');
